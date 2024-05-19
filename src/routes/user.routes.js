921618477dc8e7621550000baf14f34f3dbb5b92
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2024-05-19 17:36:26 +0530

    routes for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -8,6 +8,8 @@ import {    loginUser,
             updateAccountDetails,
             updateUserAvatar, 
             updateUserCoverImage, 
+            getUserChannelProfile,
+            getWatchHistory
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -49,7 +51,8 @@ router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvat
 router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
 
 
-
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 
 
